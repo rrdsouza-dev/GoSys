2c2219ecd3f8c2343bcc38463b75717afd486f89
rrdsouza-dev <rennator.souza@gmail.com>
2026-09-16 00:54:53 -0300

    Adicionado memory.

--- pkg/system/memory.go
+++ pkg/system/memory.go
@@ -1,1 +1,28 @@
 package system
+
+import (
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+func Memory() (float64, error) {
+	cmd := exec.Command(
+		"powershell",
+		"-Command",
+		"(Get-CimInstance Win32_OperatingSystem).FreePhysicalMemory",
+	)
+
+	output, err := cmd.Output()
+	if err != nil {
+		return 0, err
+	}
+
+	value := strings.TrimSpace(string(output))
+	freeMemory, err := strconv.ParseFloat(value, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	return freeMemory, nil
+}
