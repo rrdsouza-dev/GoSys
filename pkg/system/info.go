fa29967d79fddcdbc21295b072f78fef4b014397
rrdsouza-dev <rennator.souza@gmail.com>
2026-09-16 00:59:36 -0300

    Adicionado info (Ultimo arquivo)

--- pkg/system/info.go
+++ pkg/system/info.go
@@ -1,1 +1,21 @@
 package system
+
+import (
+	"os/exec"
+	"strings"
+)
+
+func Info() (string, error) {
+	cmd := exec.Command(
+		"powershell",
+		"-Command",
+		"Get-CimInstance Win32_OperatingSystem | Select-Object Caption, Version, OSArchitecture",
+	)
+
+	output, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+
+	return strings.TrimSpace(string(output)), nil
+}
